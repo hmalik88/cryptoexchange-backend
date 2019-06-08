16649d358101fa11e2073f7c331a1e0e945ed7ad
hmalik88 <hbmalik88@gmail.com>
2019-06-08 05:14:32 -0400

    updated rendered json to include various transfers pertaining to the current user

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -13,7 +13,9 @@ class Api::V1::AuthController < ApplicationController
 
   def show
     if current_user
-      render json: {user: current_user, transfers: current_user.transfers}
+      @god_transfers = Transfer.all.select{|transfer| transfer.user_id === 1 && transfer.recipient_id === current_user.address }
+      @inflows = Transfer.all.select{|transfer| transfer.recipient_id === current_user.address && transfer.user_id != 1}.map{|transfer| {"amount": transfer.amount, "address": User.find(transfer.user_id).address}}
+      render json: {user: current_user, transfers: current_user.transfers, god_transfers: @god_transfers, inflows: @inflows}
     else
       render json: {error: "not logged in"}, status: 422
     end
